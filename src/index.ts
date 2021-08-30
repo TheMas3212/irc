ed246714c17e7c76dd2f073c154ef3a0936109a6
TheMas3212 <mas3212@gmail.com>
2021-08-30 17:34:53 +1000

    Export additional type defs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { IRCMessage, Prefix } from './Message';
 export { TwitchClient, TwitchMessage, TwitchChannel } from './twitchClient';
 export { IRCClient as default, IRCClient, IRCChannel } from './ircClient';
+export { ClientState, TwitchOptions, IRCOptions } from './types';
